a8b7e779bcbe07db45f041325a0af2173d03f977
docker <docker@docker.home>
2021-06-28 18:27:05 +0200

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 [ -f /config/tv_grab_fr_telerama.conf ] || echo -e "yes\nall" | tv_grab_fr_telerama --configure --config-file /config/tv_grab_fr_telerama.conf
-/usr/local/bin/tv_grab_fr_telerama --config-file /config/tv_grab_fr_telerama.conf --output /data/xmltv.xml
+/usr/local/bin/tv_grab_fr_telerama --config-file /config/tv_grab_fr_telerama.conf --output /data/xmltv.xml.new && mv /data/xmltv.xml.new /data/xmltv.xml || rm /data/xmltv.xml.new 
