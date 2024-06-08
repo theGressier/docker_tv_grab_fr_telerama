258f2c1aed5a14015e32c96f2c90164c945bff9d
theGressier <pgressier@free.fr>
2024-06-08 17:18:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ GRABBERFILE=usr/local/bin/tv_grab_fr_telerama
 
 [ -f "$CONFFILE" ] || echo -e "yes\nall" | "$GRABBERFILE" --configure --config-file "$CONFFILE"
 
-REFERENCEDATE=$((`date +%s` - 86400 ))                                                                                                                                              
+REFERENCEDATE=$((`date +%s` - 43200 ))                                                                                                                                              
 [  -f "$XMLFILE" ] && XMLFILEDATE=`stat -c "%Y" "$XMLFILE"` || XMLFILEDATE=0
 if [ $XMLFILEDATE -le $REFERENCEDATE ]; then
    "$GRABBERFILE" --config-file "$CONFFILE" --output "$XMLFILE".new && mv "$XMLFILE".new "$XMLFILE" || rm "$XMLFILE".new
